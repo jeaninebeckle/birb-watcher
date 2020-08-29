bce54bfb5f2d3cd760f6de616bf52f44ca2e21d8
Jeanine Beckle <jeaninebeckle@gmail.com>
2020-08-29 11:09:14 -0500

    add links to new and specific birb pages

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Home extends React.Component {
   editBirbEvent = (e) => {
@@ -12,6 +13,8 @@ class Home extends React.Component {
       <div className="Home">
         <h1>Home</h1>
         <button className="btn btn-dark" onClick={this.editBirbEvent}>Edit A Birb</button>
+        <Link to='/new'> New Birb</Link>
+        <Link to='/birbs/birb1232'>Specific Birb</Link>
       </div>
     );
   }
